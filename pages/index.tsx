5db9432297639033559aab30614e2345b0c5f9b6
Carlos Augusto <c.augustomagnus@gmail.com>
2024-05-14 01:16:03 -0300

    meu portfolio next.js

---
+++ pages/index.tsx
@@ -0,0 +1,270 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const Fleetsummary: NextPage = () => {
+  return (
+    <div className={styles.fleetsummary}>
+      <img className={styles.fleetsummaryChild} alt="" src="/rectangle-1.svg" />
+      <header className={styles.contactInfo}>
+      <div id="Home" className={styles.carlin}><h1 className={styles.carlosdev}>Carlos.dev</h1></div>
+        <nav className={styles.userSettingsArea}>
+          <nav className={styles.serviceLinksContainer}>
+           <a id="active" href="#Home"><div className={styles.home}>{`Home`}</div></a>
+            <a id="active"  href="#About"><div className={styles.about}>{`About `}</div></a>
+             <a  href="#Contact"><div className={styles.contact}>{`Contact `}</div></a>
+          </nav>
+        </nav>
+      </header>
+      <main className={styles.content}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.additionalData}>
+            <div className={styles.notificationIconAreaParent}>
+              <div className={styles.notificationIconArea}>
+                <div className={styles.userSettings}>
+                  <div className={styles.userSettings1}>
+                    <h1 className={styles.softwareEngineer}>
+                      Software engineer
+                    </h1>
+                    <div className={styles.serviceLinkIcon}>
+                      <h1 className={styles.h1}>👋</h1>
+                    </div>
+                  </div>
+                  <div className={styles.notificationCountArea}>
+                    <div className={styles.sidebarItems}>
+                      <b className={styles.hiImCarlos}>
+                        Hi. I’m Carlos Augusto. A Passionate about programming
+                        and technology. Currently living in Criciúma, Brazil
+                      </b>
+                      <div className={styles.serviceLinks}>
+                        <div className={styles.div}>📍</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.image1Parent}>
+                    <a href="https://www.linkedin.com/in/carlosaugustodev/">
+                    <img
+                      className={styles.image1Icon}
+                      loading="lazy"
+                      alt="linkedin"
+                      src="/image-1@2x.png"
+                    /></a>
+                    <a href="https://github.com/CarlosSchefferr">
+                    <img
+                      className={styles.image2Icon}
+                      loading="lazy"
+                      alt="github"
+                      src="/image-2@2x.png"
+                    /></a>
+                  </div>
+                </div>
+              </div>
+              <img
+                className={styles.image12Icon}
+                loading="lazy"
+                alt=""
+                src="/image-12@2x.png"
+              />
+            </div>
+          </div>
+          <div className={styles.techStackContent}>
+            <div className={styles.techStackWrapper}>
+              <div className={styles.frameParent}>
+                <div className={styles.frameWrapper}>
+                  <div className={styles.accountSettingsParent}>
+                    <div id="About" className={styles.accountSettings}>
+                      <b className={styles.techStack}>Tech Stack</b>
+                    </div>
+                    <div className={styles.frameChild} />
+                  </div>
+                </div>
+                <div className={styles.image3Wrapper}>
+                  <img
+                    className={styles.image3Icon}
+                    loading="lazy"
+                    alt=""
+                    src="/image-3@2x.png"
+                  />
+                </div>
+                <div className={styles.image4Wrapper}>
+                  <img
+                    className={styles.image4Icon}
+                    alt=""
+                    src="/image-4@2x.png"
+                  />
+                </div>
+                <img
+                  className={styles.image5Icon}
+                  loading="lazy"
+                  alt=""
+                  src="/image-5@2x.png"
+                />
+                <div className={styles.image6Wrapper}>
+                  <img
+                    className={styles.image6Icon}
+                    loading="lazy"
+                    alt=""
+                    src="/image-6@2x.png"
+                  />
+                </div>
+                <div className={styles.userProfileArea}>
+                  <img
+                    className={styles.image7Icon}
+                    loading="lazy"
+                    alt=""
+                    src="/image-7@2x.png"
+                  />
+                </div>
+                <div className={styles.image9Wrapper}>
+                  <img
+                    className={styles.image9Icon}
+                    loading="lazy"
+                    alt=""
+                    src="/image-9@2x.png"
+                  />
+                </div>
+                <div className={styles.image8Wrapper}>
+                  <img
+                    className={styles.image8Icon}
+                    loading="lazy"
+                    alt=""
+                    src="/image-8@2x.png"
+                  />
+                </div>
+                <div className={styles.reportTitleArea}>
+                  <img
+                    className={styles.image10Icon}
+                    loading="lazy"
+                    alt=""
+                    src="/image-10@2x.png"
+                  />
+                </div>
+              </div>
+              <div className={styles.rectangleParent}>
+                <img
+                  className={styles.frameItem}
+                  loading="lazy"
+                  alt=""
+                  src="/rectangle-2@2x.png"
+                />
+                <div className={styles.reportDataTableWrapper}>
+                  <div className={styles.reportDataTable}>
+                    <div className={styles.reportDataTableInner}>
+                      <div className={styles.frameGroup}>
+                        <div className={styles.aboutMeWrapper}>
+                          <div className={styles.aboutMe}>
+                            <span>ABOUT ME</span>
+                            <span>{` `}</span>
+                          </div>
+                        </div>
+                        <div className={styles.hiImCarlosAugustoAPassParent}>
+                          <b className={styles.hiImCarlos1}>
+                            A Passionate about programming and technology.
+                            Currently living in Criciúma, Brazil
+                          </b>
+                          <div className={styles.wrapper}>
+                            <div className={styles.div1}>📍</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={styles.hiImCarlos2}>
+                      Since I was little, I have always been connected with
+                      technology and the world of the internet. This has always
+                      been my biggest hobby. Nowadays, I am a young student,
+                      currently studying Software Engineering at SATC
+                      university. Passionate about programming and with certain
+                      skills in HTML, CSS, Javascript, Typescript, React, node
+                      JS, Python, and Figma design.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.mainContentArea}>
+                <div className={styles.additionalDataHeading}>
+                  <div className={styles.portfolioVisitInnerWrapper}>
+                    <div className={styles.portfolioVisitInnermostWrap}>
+                      <img
+                        className={styles.capturaDeTela20240513232}
+                        alt=""
+                        src="/captura-de-tela-20240513-232255-1@2x.png"
+                      />
+                      <img
+                        className={styles.image13Icon}
+                        alt=""
+                        src="/image-13@2x.png"
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.thankYouFor}>
+                    Thank you for your visit to my personal portfolio
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.reportOptions}>
+            <div className={styles.talkToMe}>{`TALK TO ME! `}</div>
+            <div className={styles.contactWays}>CONTACT WAYS</div>
+          </div>
+          <div className={styles.footer}>
+            <div className={styles.footerWrapper}>
+              <div className={styles.frameContainer}>
+                <div className={styles.frameDiv}>
+                  <div className={styles.dataRowsParent}>
+                    <div className={styles.dataRows}>
+                      <img
+                        className={styles.icons8Whatsapp1}
+                        loading="lazy"
+                        alt=""
+                        src="/icons8whatsapp-1@2x.png"
+                      />
+                      <div className={styles.specificVehicleFilter}>
+                        <b className={styles.b}>55 51 99004-1021</b>
+                      </div>
+                    </div>
+                    <div className={styles.dataRows1}>
+                      <img
+                        className={styles.icons8NovaMensagem1}
+                        loading="lazy"
+                        alt=""
+                        src="/icons8novamensagem-1@2x.png"
+                      />
+                      <div className={styles.dataColumns}>
+                        <a
+                          className={styles.caugustomagnusgmailcom}
+                          href="mailto:c.augustomagnus@gmail.com"
+                          target="_blank"
+                        >
+                          c.augustomagnus@gmail.com
+                        </a>
+                      </div>
+                    </div>
+                    <div id="Contact" className={styles.dataRows2}>
+                      <img
+                        className={styles.icons8LogoDiscord1}
+                        loading="lazy"
+                        alt=""
+                        src="/icons8logodiscord-1@2x.png"
+                      />
+                      <div className={styles.carlosm5727Wrapper}>
+                        <b className={styles.carlosm5727}>carlosm5727</b>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.frameInner} />
+              </div>
+              <div className={styles.copyright2024CarlosAugustWrapper}>
+                <b className={styles.copyright2024}>
+                  Copyright © 2024 Carlos Augusto. All Rights Reserved.
+                </b>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Fleetsummary;
